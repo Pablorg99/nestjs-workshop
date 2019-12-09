1fa28c1c6ec6bccdfa9d61598bc099937dc208f4
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-12-09 14:17:09 +0100

    Swagger setup added

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class CreateTodoDto {
+  @ApiProperty()
   name: string;
+
+  @ApiPropertyOptional()
   description?: string;
+
+  @ApiProperty()
   done: boolean;
 }
