9fd9bd44c999575e7a61f6683ca1e9e7f2a95d52
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-12-09 13:49:55 +0100

    ToDos service coded

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,14 +1,20 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { Todo } from './interface/todo.interface';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
 export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
   @Post()
-  async create(@Body() createTodoDto: CreateTodoDto) {}
+  async create(@Body() createTodoDto: CreateTodoDto) {
+    this.todosService.create(createTodoDto);
+  }
 
   @Get()
-  async findAll(): Promise<any[]> {
-    return ['async answer'];
+  async findAll(): Promise<Todo[]> {
+    return this.todosService.findAll();
   }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { Todo } from './interface/todo.interface';
+
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  private readonly todos: Todo[] = [
+    {
+      name: 'ToDo 1',
+      description: 'ToDo 1 Description',
+      done: false,
+    },
+  ];
+
+  create(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+}
