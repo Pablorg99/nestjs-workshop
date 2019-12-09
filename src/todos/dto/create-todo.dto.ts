db9589fd5427f44a3008267a9e09b352d67a1e65
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-12-09 12:10:08 +0100

    ToDos controller coded

---
+++ src/todos/dto/create-todo.dto.ts
@@ -0,0 +1,5 @@
+export class CreateTodoDto {
+  name: string;
+  description?: string;
+  done: boolean;
+}
--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  @Post()
+  async create(@Body() createTodoDto: CreateTodoDto) {}
+
+  @Get()
+  async findAll(): Promise<any[]> {
+    return ['async answer'];
+  }
+}
